cc51d2b5eaa53d46631b89970daa874a5da35791
1Dips <ido.geva.b@gmail.com>
2024-11-05 16:35:36 +0200

    add db basics

--- main.go
+++ main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/joho/godotenv"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 )
 
 func main() {
+	envErr := godotenv.Load()
+	if envErr != nil {
+		log.Fatal("Error loading .env file")
+		return
+	}
+	sqlDsn, ok := os.LookupEnv("MASON_DATABASE_DSN")
+	if !ok {
+		log.Fatal("[Mason] Couldn't connect to database: Missing MASON_DATABASE_DSN.")
+		return
+	}
+
+	_, err := sql.Open("mysql", sqlDsn)
+	if err != nil {
+		log.Fatal("[mason] Couldn't connect to database: Is it active?", err)
+		return
+	}
 
 	http.Handle("/public/", http.FileServer(http.Dir("public")))
 
