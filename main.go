491c3d462050dabd465d2981013dc333d01e8c7e
1Dips <ido.geva.b@gmail.com>
2024-11-05 16:11:49 +0200

    add gitignore

--- main.go
+++ main.go
@@ -1,23 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := w.Write([]byte("hello World!"))
+	http.Handle("/public/", http.FileServer(http.Dir("public")))
+
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, tmpl_err := template.ParseFiles(filepath.Join("templates", "login_page.html"))
+		if tmpl_err != nil {
+			log.Println(tmpl_err)
+			return
+		}
+		err := tmpl.Execute(w, "no data needed")
 		if err != nil {
 			return
 		}
-	})
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		err_message := fmt.Errorf("[Mason] Error while creating server: %v", err)
-		fmt.Println(err_message)
-		return
-	}
 
+	})
+	log.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
